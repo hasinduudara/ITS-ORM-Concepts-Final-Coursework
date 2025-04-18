b7b4ce7124e52139ae9a9645329df43de1497142
hasinduudara <hasindutechschool@gmail.com>
2025-04-18 22:08:32 +0530

    create admin dashboard

---
+++ src/main/java/lk/ijse/gdse/mentalhealth/controller/AdminDashboardController.java
@@ -0,0 +1,79 @@
+package lk.ijse.gdse.mentalhealth.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminDashboardController {
+
+    @FXML
+    private AnchorPane AdminDashboardPage;
+
+    @FXML
+    private AnchorPane AnchorPaneAdminDashboard;
+
+    @FXML
+    private Button btnLogOut;
+
+    @FXML
+    private Button btnPatient;
+
+    @FXML
+    private Button btnPayment;
+
+    @FXML
+    private Button btnReporting;
+
+    @FXML
+    private Button btnTherapist;
+
+    @FXML
+    private Button btnTherapyProgram;
+
+    @FXML
+    private Button btnTherapySession;
+
+    @FXML
+    private Label lblAdminDashboardDate;
+
+    @FXML
+    private Label lblAdminDashboardTime;
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPatientOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPaymentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportingOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTherapistOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTherapyProgramOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTherapySessionOnAction(ActionEvent event) {
+
+    }
+
+}
